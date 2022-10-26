e6531d4c581f0590190e5fde1b8809ea625a3542
Raphael Westphal <westphal.raphael@gmail.com>
2022-10-26 20:50:14 +0200

    scripts: if no selection, fallback to whole screen

--- scripts/screens/screenshotWayland.sh
+++ scripts/screens/screenshotWayland.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p "$HOME/Screenshots/"
 name="$HOME/Screenshots/$(date +'%Y-%m-%d-%H%M%S_grim.png')"
-grim -g "$(slurp)" "$name"
+grim -g "$(slurp || slurp -o)" "$name"
 wl-copy < "$name"
