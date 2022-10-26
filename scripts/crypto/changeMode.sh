03a95c03ec376a93a7fce930c7fccb812b641fdc
Raphael Westphal <westphal.raphael@gmail.com>
2022-10-26 20:49:28 +0200

    scripts: Add support for cronos

--- scripts/crypto/changeMode.sh
+++ scripts/crypto/changeMode.sh
@@ -3,8 +3,11 @@ mode="$(cat $HOME/.scripts/crypto/mode)"
 
 if [ "$mode" = "bsc" ]; then
 	notify-send "Crypto" "Switching mode to polygon"
-	echo -n "polygon" > $HOME/.scripts/crypto/mode
+	echo -n "polygon" > "$HOME/.scripts/crypto/mode"
+elif [ "$mode" = "polygon" ]; then
+	notify-send "Crypto" "Switching mode to cronos"
+	echo -n "cronos" > "$HOME/.scripts/crypto/mode"
 else
 	notify-send "Crypto" "Switching mode to bsc"
-	echo -n "bsc" > $HOME/.scripts/crypto/mode
+	echo -n "bsc" > "$HOME/.scripts/crypto/mode"
 fi
